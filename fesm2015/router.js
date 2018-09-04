3979a232172913ac9c104f2926a6fc98b8e2b48d
Martin Ma <dxrnr-github@yahoo.com>
2018-09-04 14:18:07 +1000

    8b55dabb3 fix(platform-browser): include APP_ID in html attribute created & used by ViewEncapsulation.Emulated

--- fesm2015/router.js
+++ fesm2015/router.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+1.sha-beda212
+ * @license Angular v6.1.6+1.sha-beda212.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -6786,7 +6786,7 @@ function provideRouterInitializer() {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.6+1.sha-beda212');
+const VERSION = new Version('6.1.6+1.sha-beda212.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
